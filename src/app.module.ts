bf64d6bfcc4a8d786bd8006aa3279fa132f592df
Marlon Constante <marlon.constante@gmail.com>
2022-04-22 09:37:44 -0300

    feat(resources): added customers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), CustomersModule],
   controllers: [],
   providers: [],
 })
