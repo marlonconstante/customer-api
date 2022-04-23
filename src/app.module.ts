f82a3f8fb4ee389a84d2dcdb9073b34484516144
Marlon Constante <marlon.constante@gmail.com>
2022-04-23 00:20:20 -0300

    fix(modules): provided prisma customers repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './customers/customers.module';
-import { PrismaService } from './database/prisma';
 
 @Module({
   imports: [ConfigModule.forRoot(), CustomersModule],
-  controllers: [],
-  providers: [PrismaService],
 })
 export class AppModule {}
--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
+import { PrismaCustomersRepository } from './repositories/prisma-customers.repository';
+import { PrismaService } from '../database/prisma';
 
 @Module({
   controllers: [CustomersController],
-  providers: [CustomersService],
+  providers: [
+    CustomersService,
+    PrismaService,
+    {
+      provide: 'CustomersRepository',
+      useClass: PrismaCustomersRepository,
+    },
+  ],
 })
 export class CustomersModule {}
