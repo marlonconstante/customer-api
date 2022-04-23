bf313feb200fa2aaed6a2518db6e7fd0b388d56f
Marlon Constante <marlon.constante@gmail.com>
2022-04-23 00:09:58 -0300

    feat(services): integrated customer data

--- src/customers/customers.service.spec.ts
+++ src/customers/customers.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { InMemoryCustomersRepository } from './repositories/in-memory-customers.repository';
 
 describe('CustomersService', () => {
   let service: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [
+        CustomersService,
+        {
+          provide: 'CustomersRepository',
+          useClass: InMemoryCustomersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
+import { FindCustomersQueryDto } from './dto/find-customers-query.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { CustomersRepository } from './repositories/customers.repository';
 
 @Injectable()
 export class CustomersService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+  constructor(
+    @Inject('CustomersRepository')
+    private readonly customersRepository: CustomersRepository,
+  ) {}
+
+  async create(createCustomerDto: CreateCustomerDto) {
+    const { email } = createCustomerDto;
+    const customer = await this.customersRepository.findOne(undefined, email);
+    if (customer) throw new UnprocessableEntityException();
+
+    return this.customersRepository.create(createCustomerDto);
   }
 
-  findAll() {
-    return `This action returns all customers`;
+  findAll(findCustomersQueryDto: FindCustomersQueryDto) {
+    return this.customersRepository.findAll(findCustomersQueryDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
+  async findOne(id: string) {
+    const customer = await this.customersRepository.findOne(id);
+    if (!customer) throw new NotFoundException();
+
+    return customer;
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(id: string, updateCustomerDto: UpdateCustomerDto) {
+    await this.findOne(id);
+
+    return this.customersRepository.update(id, updateCustomerDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
+  async remove(id: string) {
+    await this.findOne(id);
+
+    return this.customersRepository.remove(id);
   }
 }
