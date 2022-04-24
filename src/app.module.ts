6eefcab59ce79cf56b640d5b707f9028751bb610
Marlon Constante <marlon.constante@gmail.com>
2022-04-24 17:31:46 -0300

    chore(dependencies): removed unused config library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CustomersModule],
+  imports: [CustomersModule],
 })
 export class AppModule {}
